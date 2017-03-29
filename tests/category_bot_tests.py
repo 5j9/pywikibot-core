3a2133f0fb5900c6c5a58fe5bafa40d5a4fae689
dalba <dalba.wiki@gmail.com>
2017-03-29 17:26:22 +0430

    category_bot_tests: Temporarily set a fake username for BaseSite
    Temporarily patch BaseSite.username method to make it return a fake username.
    This will help to circumvent NoUsername error during the initialization of
    CategoryMoveRobot if there is no user name set for wikibase.
    
    Bug: T161692
    Change-Id: I08350274a6917496ab9b1850571f216428f6b164

--- tests/category_bot_tests.py
+++ tests/category_bot_tests.py
@@ -9,16 +9,27 @@ from __future__ import absolute_import, unicode_literals
 
 __version__ = '$Id$'
 
+try:
+    from unittest.mock import patch, Mock
+except ImportError:
+    from mock import patch, Mock
+
+from pywikibot import BaseSite
 from scripts.category import CategoryMoveRobot
 
 from tests.aspects import unittest, DefaultSiteTestCase
 
 
+MOCKED_USERNAME = Mock(return_value='FakeUsername')
+
+
+# Temporarily set a username to circumvent NoUsername error; T161692
+@patch.object(BaseSite, 'username', new=MOCKED_USERNAME)
 class CfdActions(DefaultSiteTestCase):
 
     """Test CFD (Categories for deletion) actions."""
 
-    def test_strip_cfd_templates_does_nothing_when_no_templates(self):
+    def test_strip_cfd_templates_does_nothing_when_no_templates(self, ignore):
         """Test that when there are no CFD templates, the page text is not changed."""
         bot = CategoryMoveRobot(oldcat='Old', newcat='New')
         bot.newcat.text = "Nothing should change.\n\nAnother line."
@@ -26,12 +37,12 @@ class CfdActions(DefaultSiteTestCase):
         self.assertEqual(bot.newcat.text,
                          "Nothing should change.\n\nAnother line.")
 
-    def test_strip_cfd_templates_with_spaces_in_comments(self):
+    def test_strip_cfd_templates_with_spaces_in_comments(self, ignore):
         """Test that CFD templates with spaces in the syntax are removed properly."""
         self._runtest_strip_cfd_templates('<!-- BEGIN CFD TEMPLATE -->',
                                           '<!-- END CFD TEMPLATE -->')
 
-    def test_strip_cfd_templates_without_spaces_in_comments(self):
+    def test_strip_cfd_templates_without_spaces_in_comments(self, ignore):
         """Test that CFD templates without spaces in the syntax are removed properly."""
         self._runtest_strip_cfd_templates('<!--BEGIN CFD TEMPLATE-->',
                                           '<!--END CFD TEMPLATE-->')
