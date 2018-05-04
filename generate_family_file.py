wikimedia/pywikibot

commit d3cd1f4e9904e153ac58ca6d12d5c2f57a44d1a3

tox.ini: Enable E402 and disable I201 checks
Check that imports are at the top of the modules by enabling E402 checks.
Fix exising errors. Note that according to current rules, conditional
imports (inside if-else blocks) should come after normal imports.

bot.py, i18n.py, terminal_interface_base.py:
Change the `from pywikibot import config` line to
`from pywikibot import config2 as config`. This is to avoid circular
imports within pywikibot.

Disable I201 checks. Grouping imports by package name is not a PEP8
recommendation and may confuse newcomers.

With I201 being disabled, there is no other usage of flake8-import-order
package. Remove it from flake8 dependencies.

Move some __all__ variables to top, right after the __future__ imports. (PEP8)

Bug: T87409
Bug: T166950
Change-Id: I1acd2a607828d778eae611f1abdb8ab4a124de44

generate_family_file.py
@@ -15,6 +15,8 @@ import codecs
 import os
 import sys
 
+from pywikibot.site_detect import MWSite as Wiki
+
 # creating & retrieving urls
 if sys.version_info[0] > 2:
     from urllib.parse import urlparse
@@ -22,12 +24,11 @@ if sys.version_info[0] > 2:
 else:
     from urlparse import urlparse
 
+
 # Disable user-config checks so the family can be created first,
 # and then used when generating the user-config
 _orig_no_user_config = os.environ.get('PYWIKIBOT2_NO_USER_CONFIG')
 os.environ['PYWIKIBOT2_NO_USER_CONFIG'] = '2'
-
-from pywikibot.site_detect import MWSite as Wiki
 
 # Reset this flag in case another script is run by pwb after this script
 if not _orig_no_user_config:

generate_user_files.py
@@ -17,13 +17,13 @@ import sys
 from textwrap import fill
 from warnings import warn
 
+import pywikibot
+from pywikibot import config
+
+
 # Disable user-config usage as we are creating it here
 _orig_no_user_config = os.environ.get('PYWIKIBOT2_NO_USER_CONFIG')
 os.environ['PYWIKIBOT2_NO_USER_CONFIG'] = '2'
-
-import pywikibot
-
-from pywikibot import config
 
 # Reset this flag in case another script is run by pwb after this script
 if not _orig_no_user_config:

docs/conf.py
@@ -21,6 +21,9 @@ import os
 from os.path import abspath, dirname, join
 import sys
 
+from scripts.cosmetic_changes import warning
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -28,8 +31,6 @@ docs_dir = dirname(__file__)
 repo_dir = abspath(join(docs_dir, '..'))
 sys.path.insert(0, repo_dir)
 os.chdir(repo_dir)
-
-from scripts.cosmetic_changes import warning
 
 # -- General configuration -----------------------------------------------------
 

pywikibot/__init__.py
@@ -12,30 +12,15 @@ __url__ = 'https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Pywikibot'
 
 import atexit
 import datetime
+from decimal import Decimal
 import math
 import re
 import sys
 import threading
 
-from decimal import Decimal
-
-if sys.version_info[0] > 2:
-    from queue import Queue
-    long = int
-    basestring = str
-else:
-    from Queue import Queue
-
 from warnings import warn
 
-# logging must be imported first so that other modules can
-# use these logging methods during the initialisation sequence.
-from pywikibot.logging import (
-    critical, debug, error, exception, log, output, stdout, warning
-)
-
-from pywikibot import config2 as config
-
+from pywikibot._wbtypes import WbRepresentation as _WbRepresentation
 from pywikibot.bot import (
     input, input_choice, input_yn, inputChoice, handle_args, showHelp, ui,
     calledModuleName, Bot, CurrentPageBot, WikidataBot,
@@ -45,6 +30,7 @@ from pywikibot.bot import (
 from pywikibot.bot_choice import (
     QuitKeyboardInterrupt as _QuitKeyboardInterrupt,
 )
+from pywikibot import config2 as config
 from pywikibot.data.api import UploadWarning as _UploadWarning
 from pywikibot.diff import PatchManager
 from pywikibot.exceptions import (
@@ -65,7 +51,11 @@ from pywikibot.exceptions import (
 )
 from pywikibot.family import Family
 from pywikibot.i18n import translate
+from pywikibot.logging import (
+    critical, debug, error, exception, log, output, stdout, warning
+)
 from pywikibot.site import BaseSite
+import pywikibot.textlib as textlib
 from pywikibot.tools import (
     # __ to avoid conflict with ModuleDeprecationWrapper._deprecated
     classproperty,
@@ -80,7 +70,14 @@ from pywikibot.tools import (
 )
 from pywikibot.tools.formatter import color_format
 
-import pywikibot.textlib as textlib
+
+if sys.version_info[0] > 2:
+    from queue import Queue
+    long = int
+    basestring = str
+else:
+    from Queue import Queue
+
 
 textlib_methods = (
     'unescape', 'replaceExcept', 'removeDisabledParts', 'removeHTMLParts',
@@ -254,9 +251,6 @@ class Timestamp(datetime.datetime):
                              newdt.tzinfo)
         else:
             return newdt
-
-
-from pywikibot._wbtypes import WbRepresentation as _WbRepresentation
 
 
 class Coordinate(_WbRepresentation):
@@ -1293,7 +1287,7 @@ getSite = redirect_func(Site, old_name='getSite')
 
 
 # These imports depend on Wb* classes above.
-from pywikibot.page import (
+from pywikibot.page import (  # noqa: E402
     Page,
     FilePage,
     Category,
@@ -1303,7 +1297,8 @@ from pywikibot.page import (
     PropertyPage,
     Claim,
 )
-from pywikibot.page import html2unicode, url2unicode, unicode2html
+from pywikibot.page import (  # noqa: E402
+    html2unicode, url2unicode, unicode2html)
 
 
 link_regex = re.compile(r'\[\[(?P<title>[^\]|[<>{}]*)(\|.*?)?\]\]')

pywikibot/bot.py
@@ -58,6 +58,29 @@ from __future__ import absolute_import, unicode_literals
 # class definition that can be subclassed to create new, functional bot
 # scripts, instead of writing each one from scratch.
 
+__all__ = (
+    'CRITICAL', 'ERROR', 'INFO', 'WARNING', 'DEBUG', 'INPUT', 'STDOUT',
+    'VERBOSE', 'critical', 'debug', 'error', 'exception', 'log', 'warning',
+    'output', 'stdout', 'LoggingFormatter', 'RotatingFileHandler',
+    'init_handlers', 'writelogheader',
+    'input', 'input_choice', 'input_yn', 'inputChoice', 'input_list_choice',
+    'Option', 'StandardOption', 'NestedOption', 'IntegerOption',
+    'ContextOption', 'ListOption', 'OutputProxyOption',
+    'HighlightContextOption', 'ChoiceException', 'UnhandledAnswer',
+    'Choice', 'AlwaysChoice',
+    'QuitKeyboardInterrupt',
+    'InteractiveReplace',
+    'calledModuleName', 'handle_args', 'handleArgs',
+    'showHelp', 'suggest_help',
+    'writeToCommandLogFile', 'open_webbrowser',
+    'OptionHandler',
+    'BaseBot', 'Bot', 'SingleSiteBot', 'MultipleSitesBot',
+    'CurrentPageBot', 'AutomaticTWSummaryBot',
+    'ExistingPageBot', 'FollowRedirectPageBot', 'CreatingPageBot',
+    'RedirectPageBot', 'NoRedirectPageBot',
+    'WikidataBot',
+)
+
 # Note: all output goes thru python std library "logging" module
 
 import codecs
@@ -69,15 +92,11 @@ import os
 import sys
 import time
 import warnings
+from warnings import warn
 import webbrowser
 
-from warnings import warn
-
-_logger = "bot"
-
 import pywikibot
-
-from pywikibot import config
+from pywikibot import config2 as config
 from pywikibot import daemonize
 from pywikibot import i18n
 from pywikibot import version
@@ -104,28 +123,8 @@ from pywikibot.tools._logging import (
 )
 from pywikibot.tools.formatter import color_format
 
-__all__ = (
-    'CRITICAL', 'ERROR', 'INFO', 'WARNING', 'DEBUG', 'INPUT', 'STDOUT',
-    'VERBOSE', 'critical', 'debug', 'error', 'exception', 'log', 'warning',
-    'output', 'stdout', 'LoggingFormatter', 'RotatingFileHandler',
-    'init_handlers', 'writelogheader',
-    'input', 'input_choice', 'input_yn', 'inputChoice', 'input_list_choice',
-    'Option', 'StandardOption', 'NestedOption', 'IntegerOption',
-    'ContextOption', 'ListOption', 'OutputProxyOption',
-    'HighlightContextOption', 'ChoiceException', 'UnhandledAnswer',
-    'Choice', 'AlwaysChoice',
-    'QuitKeyboardInterrupt',
-    'InteractiveReplace',
-    'calledModuleName', 'handle_args', 'handleArgs',
-    'showHelp', 'suggest_help',
-    'writeToCommandLogFile', 'open_webbrowser',
-    'OptionHandler',
-    'BaseBot', 'Bot', 'SingleSiteBot', 'MultipleSitesBot',
-    'CurrentPageBot', 'AutomaticTWSummaryBot',
-    'ExistingPageBot', 'FollowRedirectPageBot', 'CreatingPageBot',
-    'RedirectPageBot', 'NoRedirectPageBot',
-    'WikidataBot',
-)
+
+_logger = 'bot'
 
 if not PY2:
     unicode = str

pywikibot/botirc.py
@@ -20,6 +20,8 @@ from __future__ import absolute_import, unicode_literals
 
 import re
 
+import pywikibot
+
 try:
     from ircbot import SingleServerIRCBot
 except ImportError as e:
@@ -35,8 +37,6 @@ except ImportError as e:
 
 
 _logger = "botirc"
-
-import pywikibot
 
 
 class IRCBot(pywikibot.Bot, SingleServerIRCBot):

pywikibot/comms/http.py
@@ -29,6 +29,24 @@ from warnings import warn
 
 import requests
 
+from pywikibot import __release__
+from pywikibot.bot import calledModuleName
+from pywikibot.comms import threadedhttp
+from pywikibot import config
+from pywikibot.exceptions import (
+    FatalServerError, Server504Error, Server414Error
+)
+from pywikibot.logging import critical, debug, error, log, warning
+from pywikibot.tools import (
+    deprecated,
+    deprecate_arg,
+    file_mode_checker,
+    issue_deprecation_warning,
+    PY2,
+    StringTypes,
+)
+import pywikibot.version
+
 try:
     import requests_oauthlib
 except ImportError as e:
@@ -42,25 +60,6 @@ else:
     from urllib2 import quote
     from urlparse import urlparse
 
-from pywikibot import config
-
-from pywikibot import __release__
-from pywikibot.bot import calledModuleName
-from pywikibot.comms import threadedhttp
-from pywikibot.exceptions import (
-    FatalServerError, Server504Error, Server414Error
-)
-from pywikibot.logging import critical, debug, error, log, warning
-from pywikibot.tools import (
-    deprecated,
-    deprecate_arg,
-    file_mode_checker,
-    issue_deprecation_warning,
-    PY2,
-    StringTypes,
-)
-
-import pywikibot.version
 
 # The error message for failed SSL certificate verification
 # 'certificate verify failed' is a commonly detectable string

pywikibot/comms/threadedhttp.py
@@ -15,14 +15,14 @@ import codecs
 import re
 import sys
 
+import pywikibot
+from pywikibot.tools import deprecated, UnicodeMixin
+
 if sys.version_info[0] > 2:
     from urllib.parse import urlparse
 else:
     from urlparse import urlparse
 
-import pywikibot
-
-from pywikibot.tools import deprecated, UnicodeMixin
 
 _logger = "comm.threadedhttp"
 

5j9/pywikibot-core

commit d7d2307191361022f41dc3c11c92efc233b0407b

Fix regression in generate_user_files.py and generate_family_file.py
The regression was introduced in d3cd1f4e9904e153ac58ca6d12d5c2f57a44d1a3.

Change-Id: Iebc7188f387c7860122417a23925a2432d68d126

generate_family_file.py
@@ -15,8 +15,6 @@ import codecs
 import os
 import sys
 
-from pywikibot.site_detect import MWSite as Wiki
-
 # creating & retrieving urls
 if sys.version_info[0] > 2:
     from urllib.parse import urlparse
@@ -24,11 +22,12 @@ if sys.version_info[0] > 2:
 else:
     from urlparse import urlparse
 
-
 # Disable user-config checks so the family can be created first,
 # and then used when generating the user-config
 _orig_no_user_config = os.environ.get('PYWIKIBOT2_NO_USER_CONFIG')
 os.environ['PYWIKIBOT2_NO_USER_CONFIG'] = '2'
+
+from pywikibot.site_detect import MWSite as Wiki  # noqa: E402
 
 # Reset this flag in case another script is run by pwb after this script
 if not _orig_no_user_config:

generate_user_files.py
@@ -17,13 +17,12 @@ import sys
 from textwrap import fill
 from warnings import warn
 
-import pywikibot
-from pywikibot import config
-
-
 # Disable user-config usage as we are creating it here
 _orig_no_user_config = os.environ.get('PYWIKIBOT2_NO_USER_CONFIG')
 os.environ['PYWIKIBOT2_NO_USER_CONFIG'] = '2'
+
+import pywikibot  # noqa: E402
+from pywikibot import config  # noqa: E402
 
 # Reset this flag in case another script is run by pwb after this script
 if not _orig_no_user_config: